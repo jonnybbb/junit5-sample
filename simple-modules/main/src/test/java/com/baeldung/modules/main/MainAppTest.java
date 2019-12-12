cb49f2ce3e4c408562d10fce3a440399fcaa207f
dieter <dieter.holz@fhnw.ch>
2019-12-12 13:39:30 +0100

    build.gradle ergaenzt damit normale Unit-Tests moeglich sind

---
+++ simple-modules/main/src/test/java/com/baeldung/modules/main/MainAppTest.java
@@ -0,0 +1,16 @@
+package com.baeldung.modules.main;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Dieter Holz
+ */
+class MainAppTest {
+    @Test
+    void testSetup(){
+        assertTrue(true);
+    }
+
+}
